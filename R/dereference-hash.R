59ead795c3b2e8c550b6fd0006e2908ae30f68ba
Danny Kaplan <kaplan@macalester.edu>
2021-09-24 09:28:21 -0600

    Moved functions to Zcalc package. Added stub for dereferencing hash IDs.

---
+++ R/dereference-hash.R
@@ -0,0 +1,11 @@
+#' Dereference a hash code into a link and a number.
+#'
+#' @param hash The alphanumeric character string identifying the object.
+#' @export
+dereference_hash <- function(hash) {
+  # This is just a stub
+  # We need to implement a system for collecting hash codes that the
+  # other label/URL they identify.
+  if (hash == "jAOmME") list(URL="none yet", number="26.16")
+  else list(URL="none yet", number = hash)
+}
