433c37ccc5c04111ea893a95340577a9048fd346
Danny Kaplan <kaplan@macalester.edu>
2021-12-21 13:24:19 -0600

    Added program for abbreviating tables.

---
+++ R/and_so_on.R
@@ -0,0 +1,20 @@
+#' Formats tables to show top and bottom rows.
+#'
+#' @param data A data frame to display
+#' @param top An integer. How many of the first rows to show.
+#' @param bottom Like `top`, for for the bottom rows.
+#' @param message Character string to place between the top and bottom rows.
+#'
+#' @export
+and_so_on <- function(data, top=3, bottom=2, message="... and so on ...") {
+  ncolumns <- length(data)
+  Top <- knitr::kable(head(data, top), format="html") |> strsplit("\n") |> unlist()
+  Bottom <- knitr::kable(tail(data, bottom), format="html") |> strsplit("\n") |> unlist()
+  topdrop <- grep("</table>|</tbody>", Top)
+  bottomdrop <- grep("<table>|<thead>|</thead>|</th>|<tbody>", Bottom)
+  #Bottom <- gsub("\\<thead\\>.*\\<\\thead>", "", Bottom)
+  middle <- glue::glue("  <tr><td colspan={ncolumns} align=\"center\">{message}</td></tr>")
+  res <- paste(c(Top[-topdrop], middle, Bottom[-bottomdrop]), collapse="\n")
+
+  structure(res, format = "html", class = "knitr_kable")
+}
