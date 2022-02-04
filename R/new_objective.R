5dd1384746391df655cf9f3ea4c6bcc0d4957562
Danny Kaplan <kaplan@macalester.edu>
2022-02-03 18:37:07 -0700

    Adding skill-matching functions and addins

--- R/new_objective.R
+++ R/new_objective.R
@@ -27,6 +27,36 @@ new_objective <- function() {
     id = this_doc$id)
 }
 
+#' An addin for putting a skills template in a document.
+#' @export
+add_skills <- function() {
+  this_doc <- rstudioapi::getActiveDocumentContext()
+  contents <- this_doc$contents
+  ID_raw <- rstudioapi::primary_selection(this_doc$selection)$text
+  if (nchar(ID_raw) == 0) stop("Must select text giving a skill ID.")
+  skill_text <- check_skill(ID_raw) %>% gsub("\n$", "", .)
+  if (nchar(skill_text) > 0) {
+    hash <- paste0("Skill-link-",
+                   stringi::stri_rand_strings(1, 5, pattern = "[A-Z0-9]"),
+                   collapse="")
+    text <- glue::glue("`r skill_point(\"{ID_raw}\", hash=\"{hash}\")`\n<!--Skill {ID_raw}:   {skill_text}  -->\n")
+    rstudioapi::insertText( text,
+                           id = this_doc$id)
+  }
+}
+
+#' An addin for putting a skills template in a document.
+#' @export
+add_hash <- function() {
+  this_doc <- rstudioapi::getActiveDocumentContext()
+  contents <- this_doc$contents
+  hash <- paste0(sample(LETTERS, 1), "-",
+                 stringi::stri_rand_strings(1, 7, pattern = "[A-Z0-9]"),
+                 collapse="")
+
+  rstudioapi::insertText(hash,
+                         id = this_doc$id)
+}
 #'
 construct_yaml <- function(newID, topic, subtopic,fname, text="Indent text") {
   glue::glue(
@@ -39,6 +69,38 @@ construct_yaml <- function(newID, topic, subtopic,fname, text="Indent text") {
     '- file: {fname}\n',
     '- ID: {newID}\n',
     '- successor: none\n\n\n')
+}
+
+# Create a data frame of objectives
+Skill_env <- new.env()
+#' @export
+skills_table <- function() {
+  # Check if this has already been done
+  if ("Skills" %in% names(Skill_env)) return(Skill_env$Skills)
+  # If not, read in the files
+  yml_files <- dir(system.file("Objectives", package="Znotes"),
+                  full.names=TRUE)
+  keepers <- yml_files[grepl(".yml$|.yaml$", yml_files)]
+  Skills <- tibble()
+  for (fname in keepers) {
+    tmp <- objective2dataframe(fname)
+    Skills <- dplyr::bind_rows(Skills, tmp)
+  }
+  assign("Skills", Skills, envir = Skill_env)
+  Skills
+}
+
+#' @export
+check_skill <- function(txt) {
+  Skills <- skills_table()
+  if (txt %in% Skills$ID) {
+    rownum <- which(Skills$ID == txt)
+    return(Skills[[rownum, "text"]])
+  } else {
+    best <- stringdist::amatch(txt, Skills$ID ,maxDist=3)
+    message("Maybe this one:", Skills$ID[best], "\n", Skills$text[best])
+    return("")
+  }
 }
 
 #' @export
