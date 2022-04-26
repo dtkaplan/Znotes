a0cb32bacac86259814b0e4ee0ef43c4dd4339fb
Daniel Kaplan <dtkaplan@gmail.com>
2022-04-26 11:09:13 -0600

    Starting new utilities for exercise typesetting.

--- R/typeset-exercises.R
+++ R/typeset-exercises.R
@@ -6,34 +6,39 @@
 #' @param start_num Integer > 0 or letter "a" to "z" specifying where to start in the label sequence
 #' @export
 format_exercises <- function(
-    Roster, chap=99, start_num=1,
+    Roster,
     label_format = c("numeric", "alpha", "ALPHA", "roman", "ROMAN"),
     template = "## Exer. {number}\n\n **Exercise {number}**\n\n{contents}\n\n") {
 
-  label_format <- match.arg(label_format)
-  # Generate a sequence of labels, either numeric or alpha
-  if (nrow(Roster) > 3000) stop("Maximum of 3000 exercises in one call.")
-  letters[12] <- "L" # Don't use lowercase L
-  labels <- switch(label_format,
-                   numeric = 1:3000,
-                   alpha = c(letters,
-                             c(outer(letters, 1:99, paste0)),
-                             c(outer(letters, letters, paste0))),
-                   ALPHA = c(LETTERS,
-                             c(outer(LETTERS, 1:99, paste0)),
-                             c(outer(LETTERS, LETTERS, paste0))),
-                   roman = tolower(utils::as.roman(1:3000)),
-                   ROMAN = utils::as.roman(1:3000)
-  )
-  start_num <- which(labels == start_num)
-  labels <- paste0(chap, ".", labels)
+  # label_format <- match.arg(label_format)
+  # # Generate a sequence of labels, either numeric or alpha
+  # if (nrow(Roster) > 3000) stop("Maximum of 3000 exercises in one call.")
+  # letters[12] <- "L" # Don't use lowercase L
+  # labels <- switch(label_format,
+  #                  numeric = 1:3000,
+  #                  alpha = c(letters,
+  #                            c(outer(letters, 1:99, paste0)),
+  #                            c(outer(letters, letters, paste0))),
+  #                  ALPHA = c(LETTERS,
+  #                            c(outer(LETTERS, 1:99, paste0)),
+  #                            c(outer(LETTERS, LETTERS, paste0))),
+  #                  roman = tolower(utils::as.roman(1:3000)),
+  #                  ROMAN = utils::as.roman(1:3000)
+  # )
+  # start_num <- which(labels == start_num)
+  # labels <- paste0(chap, ".", labels)
 
   Res <- character(nrow(Roster))
-  count <- start_num
+
   for (k in 1:nrow(Roster)) {
-    Res[k] <- add_exercise(Roster$fname[k], labels[count],
-                           template=template)
-    count <- count+1
+    markup <- try(add_exercise(paste0(Roster$block[k],"/", Roster$fname[k]),
+                           Roster$number[k],
+                           template=template))
+    Res[k] <- if (inherits(markup, "try-error")) {
+      glue::glue("\n\nProblem with {Roster$block[k]} {Roster$fname[k]}\n\n")
+    } else {
+      markup
+    }
   }
 
   cat(paste(Res, collapse="\n\n"))
